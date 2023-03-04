5044d5b46f82b4a53e4f4957da074b57c51afeda
Karel Nagel <nagelkarel@gmail.com>
2023-03-04 19:52:45 +0200

    changed prompt

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const defaultPrompt = (name: string) =>
-  `I want you to act like ${name}. I want you to respond and answer like ${name} using the tone, manner and vocabulary ${name} would use. Do not write any explanations. Only answer like ${name}. You must know all of the knowledge of ${name}.`;
+  `I want you to act like ${name}. I want you to respond and answer like ${name} using the tone, manner and vocabulary ${name} would use. Do not write any explanations. Only answer like ${name}. You must know all of the knowledge of ${name}. This is a casual conversation between ${name} and user, so use natural language.`;
